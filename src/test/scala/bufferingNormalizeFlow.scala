d7fb1366776acac316244ffdf9ad2cc594dd4d00
wwagner4 <wwagner4@gmail.com>
2015-08-30 21:13:21 +0200

    extract bufferingNormalizeFlow

---
+++ src/test/scala/bufferingNormalizeFlow.scala
@@ -0,0 +1,57 @@
+import akka.stream.scaladsl._
+import akka.stream._
+import akka.actor._
+import scala.concurrent._
+import scala.concurrent.duration._
+import akka.stream.stage._
+import scala.concurrent.forkjoin.ThreadLocalRandom
+
+object BufferingNormalizeFlow {
+
+  // Transforms a stream of integers to their sum
+  val maxFlow: Flow[Int, Int, Future[Int]] = {
+    import FlowGraph.Implicits._
+    val maxSink: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)((cuml, elem) => if (elem > cuml) elem else cuml)
+    Flow(maxSink) {
+      implicit builder =>
+        fold =>
+          (fold.inlet, builder.materializedValue.mapAsync(4)(identity).outlet)
+    }
+  }
+
+  // Takes the first element of a stream and transforms it into an endless stream of that element.
+  class Fill[A]() extends StatefulStage[A, A] {
+    override def initial: StageState[A, A] =
+      new StageState[A, A] {
+        override def onPush(elem: A, ctx: Context[A]): SyncDirective = {
+          val iter = new Iterator[A] {
+            def hasNext = true;
+            def next = elem
+          }
+          emit(iter, ctx)
+        }
+      }
+  }
+
+  def create: Flow[Int, Double, _] = {
+
+    val BUFFER_SIZE = 1600
+
+    Flow() { implicit b =>
+      import FlowGraph.Implicits._
+      val bcast = b.add(Broadcast[Int](2))
+      val zip = b.add(Zip[Int, Int]())
+      val max = b.add(maxFlow)
+      val fill = b.add(Flow[Int].transform(() => new Fill[Int]()))
+      val norm = b.add(Flow[(Int, Int)].map { case (value, max) => value.toDouble / max })
+      val buffer = b.add(Flow[Int].buffer(BUFFER_SIZE, OverflowStrategy.fail))
+
+      bcast ~> buffer ~> zip.in0
+      bcast ~> max ~> fill ~> zip.in1
+      zip.out ~> norm
+
+      (bcast.in, norm.outlet)
+    }
+  }
+
+}
--- src/test/scala/normalize.scala
+++ src/test/scala/normalize.scala
@@ -10,51 +10,6 @@ import scala.concurrent.forkjoin.ThreadLocalRandom
 
 class NormalizeSuite extends FunSuite with BeforeAndAfterEach {
 
-  // Transforms a stream of integers to their sum
-  val maxFlow: Flow[Int, Int, Future[Int]] = {
-    import FlowGraph.Implicits._
-    val maxSink: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)((cuml, elem) => if (elem > cuml) elem else cuml)
-    Flow(maxSink) {
-      implicit builder =>
-        fold =>
-          (fold.inlet, builder.materializedValue.mapAsync(4)(identity).outlet)
-    }
-  }
-
-  // Takes the first element of a stream and transforms it into an endless stream of that element.
-  class Fill[A]() extends StatefulStage[A, A] {
-    override def initial: StageState[A, A] =
-      new StageState[A, A] {
-        override def onPush(elem: A, ctx: Context[A]): SyncDirective = {
-          val iter = new Iterator[A] {
-            def hasNext = true;
-            def next = elem
-          }
-          emit(iter, ctx)
-        }
-      }
-  }
-
-  val bufferingNormalizeFlow: Flow[Int, Double, _] = {
-
-    val BUFFER_SIZE = 1600
-
-    Flow() { implicit b =>
-      import FlowGraph.Implicits._
-      val bcast = b.add(Broadcast[Int](2))
-      val zip = b.add(Zip[Int, Int]())
-      val max = b.add(maxFlow)
-      val fill = b.add(Flow[Int].transform(() => new Fill[Int]()))
-      val norm = b.add(Flow[(Int, Int)].map { case (value, max) => value.toDouble / max })
-      val buffer = b.add(Flow[Int].buffer(BUFFER_SIZE, OverflowStrategy.fail))
-
-      bcast ~> buffer ~> zip.in0
-      bcast ~> max ~> fill ~> zip.in1
-      zip.out ~> norm
-
-      (bcast.in, norm.outlet)
-    }
-  }
   
   def randomIntegersSource(size: Int): Source[Int, _] = {
     val iter = Iterator.continually(ThreadLocalRandom.current().nextInt(100))
@@ -62,25 +17,42 @@ class NormalizeSuite extends FunSuite with BeforeAndAfterEach {
   }
   
   test("normalize a stream of integers") {
-
-    implicit val sys = ActorSystem("sys")
-    try {
-      implicit val materializer = ActorMaterializer()
+    def test(m: Materializer): Option[Future[_]] = {
+      implicit val materializer = m
 
       val src: Source[Int, _] = randomIntegersSource(size = 20)
       
       // Converts a stream of positive integers to doubles ranging from 0 to 1.
-      // The gratest input value converts to 1
-      val normalizeFlow: Flow[Int, Double, _] = bufferingNormalizeFlow
-      
-      
-      val f = src.via(normalizeFlow).runForeach {norm => println("%.3f" format norm)}
+      // The greatest input value converts to 1
+      val normalizeFlow: Flow[Int, Double, _] = BufferingNormalizeFlow.create
+      Some(src.via(normalizeFlow).runForeach {norm => println("%.3f" format norm)})
+    }
+    runTest(test)
+  }
 
-      Await.result(f, 2.second)
+  
+  trait SourceContainer[T, M] {
+    def source: Source[T, M]
+  }
+  
+  test("normalize a stream of source containers") {
+    def test(m: Materializer): Option[Future[_]] = {
+      None
+    }
+    runTest(test)
+  }
+  
+  protected def runTest(f: Materializer => Option[Future[_]]) = {
+    implicit val sys = ActorSystem("sys")
+    try {
+      val m = ActorMaterializer()
+      f(m) match {
+        case Some(f) => Await.result(f, 2.second)
+        case None => ()
+      }
     } finally {
       sys.shutdown()
     }
-
   }
 
 }
