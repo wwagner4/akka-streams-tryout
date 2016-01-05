e61207c30b27e5b5261f2de3dc02b4518a810f94
wwagner4 <wwagner4@gmail.com>
2016-01-05 08:31:50 +0100

    added zip with cumulated

--- src/test/scala/ZipWithCumulated.scala
+++ src/test/scala/ZipWithCumulated.scala
@@ -5,6 +5,27 @@ import akka.stream.stage._
 
 trait ZipWithCumulated[E, C] {
 
+  class Fill[T] extends DetachedStage[T, T] {
+    private var currentValue: T = _
+    private var waitingFirstValue = true
+
+    override def onPush(elem: T, ctx: DetachedContext[T]): UpstreamDirective = {
+      currentValue = elem
+      waitingFirstValue = false
+      if (ctx.isHoldingDownstream) ctx.pushAndPull(currentValue)
+      else ctx.pull()
+    }
+
+    override def onPull(ctx: DetachedContext[T]): DownstreamDirective = {
+      if (waitingFirstValue) ctx.holdDownstream()
+      else ctx.push(currentValue)
+    }
+
+    override def onUpstreamFinish(ctx: DetachedContext[T]): TerminationDirective = {
+      ctx.absorbTermination()
+    }
+  }
+
   import GraphDSL.Implicits._
 
   def bufferSize: Int
